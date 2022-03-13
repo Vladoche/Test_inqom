bb3e3d8d7ad4fbfb21e71fea1793192b2d023a34
Vladoche <vladimir.fouillade@hotmail.fr>
2022-03-13 13:40:36 +0100

    Upload finalisé

--- cypress/integration/itw_wttj.js
+++ cypress/integration/itw_wttj.js
@@ -1,9 +1,36 @@
-describe(`after visiting ["www.welcometothejungle.com/fr/me/settings/account"] webpage, clicking on ["Se connecter"] button, filling ["Email", "Mot de passe"] inputs, clicking ["Se connecter"] button, filling ["Photo de profil"] input and clicking on ["OK"] button`, () => {
+describe(`Changement de la photo de profil dans les paramètres`, () => {
   before(() => {
     cy.visit('fr/me/settings/account')
-    // ...
+
+    cy.fixture('ids/data').then((data) => {
+
+      cy.get('.sc-igXgud > [data-testid="login-field-email"]').type(data.email)
+      cy.get('[data-testid="login-field-password"]').type(data.password)
+      cy.get('[data-testid="login-button-submit"]').click()
+
+    })
+
   })
   it("_profile-avatar data are updated", () => {
+
+    cy.get('#axeptio_btn_acceptAll').click()
+
+    cy.get('.dfcLOV').attachFile('img/inqom.png', { subjectType: 'drag-n-drop' })
+    cy.get('.sc-hkgtus')
+    cy.get('.sc-hlGDCY').should('not.contain', 'Ajouter un fichier').then(() => {
+        cy.get('[data-testid="account-edit-button-submit"]').click()
+      })
+
+      cy.wait(3000)
+      cy.reload()
+
+    cy.get('.bZzTqu > .sc-fyrocj').click().then(() => {
+      cy.get('[data-testid="account-edit-button-submit"]').click()
+      cy.get('.sc-hkgtus').should('contain', 'Mise à jour réussie !')
+      cy.contains('Ajouter un fichier').should('be.visible')
+    })
+
+
     // ....
   })
 })
